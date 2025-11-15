39098ff86ad23d9ba7305c368fa1f8b0317db05c
Vihanga Indusara <vihangadev@gmail.com>
2025-11-16 05:19:13 +0530

    Revert "Merge branch 'temp' into lesanda"
    This reverts commit d040c4e87520aee71c60ded0f09fe4d1670490d7, reversing
    changes made to 521ec6ba1e2b7cdfcfc307fe7bcb14e651ce758b.

---
+++ app/src/main/java/com/cosmoscout/ui/places/PlacesFragment.java
@@ -0,0 +1,14 @@
+package com.cosmoscout.ui.places;
+
+import com.cosmoscout.R;
+import com.cosmoscout.ui.RefreshableFragment;
+
+/**
+ * Simple placeholder fragment for the Places tab.
+ */
+public class PlacesFragment extends RefreshableFragment {
+
+    public PlacesFragment() {
+        super(R.layout.fragment_places);
+    }
+}
