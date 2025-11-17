efd73ab883e28094a33293c26b4b7084cd0eb566
Sithuja <sa24610981@my.sliit.lk>
2025-11-17 21:07:50 +0530

    Created the homapage of the application HomeFragment.java including BestHourUiModel and ApodUiModel

---
+++ app/src/main/java/com/cosmoscout/ui/home/BestHourUiModel.java
@@ -0,0 +1,66 @@
+package com.cosmoscout.ui.home;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
+
+/**
+ * View payload for the Best Hour Tonight card.
+ */
+public class BestHourUiModel {
+    private final String statusLabel;
+    private final String timeRange;
+    private final String conditions;
+    private final String locationName;
+    private final String footer;
+    @DrawableRes
+    private final int statusBackgroundRes;
+    @ColorRes
+    private final int statusTextColorRes;
+
+    public BestHourUiModel(String statusLabel,
+                           String timeRange,
+                           String conditions,
+                           String locationName,
+                           String footer,
+                           @DrawableRes int statusBackgroundRes,
+                           @ColorRes int statusTextColorRes) {
+        this.statusLabel = statusLabel;
+        this.timeRange = timeRange;
+        this.conditions = conditions;
+        this.locationName = locationName;
+        this.footer = footer;
+        this.statusBackgroundRes = statusBackgroundRes;
+        this.statusTextColorRes = statusTextColorRes;
+    }
+
+    public String getStatusLabel() {
+        return statusLabel;
+    }
+
+    public String getTimeRange() {
+        return timeRange;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    @DrawableRes
+    public int getStatusBackgroundRes() {
+        return statusBackgroundRes;
+    }
+
+    @ColorRes
+    public int getStatusTextColorRes() {
+        return statusTextColorRes;
+    }
+}
+
